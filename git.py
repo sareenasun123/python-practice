35d59462be929a6c74a47e201d3913d0f005f047
sareenasun123 <sunuwarsareena@gmailcom>
2026-05-12 17:16:24 +0545

    uncomment games.py

--- git.py
+++ git.py
@@ -8,6 +8,23 @@ remote url
 remote repo
 local repo
 
+'''
 
+
+
+
+'''
+
+git init .
+make a file .gitignore and add any file if needed
+git add .
+git commit -m "message"
+
+git remote add origin <remote_url>
+
+check branch
+git branch
+
+git push origin <branch>
 
 '''
