0f275c580e46e81aa7e9d671c86ffc8ce9e6c0e6
sareenasun123 <sunuwarsareena@gmailcom>
2026-05-21 18:10:01 +0545

    practice revision of basic python

---
+++ revision.py
@@ -0,0 +1,90 @@
+a = 10
+b = 100
+a = "hello"
+b = 23.5
+c = False
+a = None
+data= "100"
+
+DATA = "broadway"
+DaTa = "hello"
+data2 = 1
+data2hello = 1
+data_1 = 1
+_data_dada_ = 500
+
+print("_data_dada_")
+print(_data_dada_)
+print(a)
+print(data)
+
+a = 10
+print(type(a))
+
+a = 12.5
+print(type(a))
+
+a = "hello"
+print(type(a))
+
+a = False
+print(type(a))
+
+a = None
+print(type(a))
+
+a = "100"
+print(isinstance(a,int))
+
+a = "100"
+print(type(a))
+b = int(a)
+print(type(b))
+
+
+# a = "broadway"
+# b = int(a)
+
+a = "1"
+print(type(a))
+b = float(a)
+print(type(b))
+
+
+# + - * / % ** //
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(5/2)
+
+print(2.5 + 3.5)
+print("hello"+"world")
+
+print("hello"+"1")
+print("test"*5)
+
+
+# %
+print(10%3)
+
+
+print(2**32)
+
+print(10//3)
+
+
+
+print(2=="2")
+
+print("test"=="Test")
+
+print(1>5)
+print(5<100)
+
+
+print(1>=5)
+print(5<=5)
+
+
+print("test">1)
